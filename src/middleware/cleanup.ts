10f83e95cb70bc455e28aef213e37d15250d5ea4
Alessian Dormal <alessian@alessian.be>
2025-10-22 16:57:24 +0200

    fix small mistake in the cleanup script (#9)

--- src/middleware/cleanup.ts
+++ src/middleware/cleanup.ts
@@ -2,28 +2,24 @@ import { NextFunction, Response, Request } from "express";
 import { getLogger } from "../utils/Logger";
 import { getDbHandler } from "../db/DbHandler";
 
-export const cleanup = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  const userId = req.body.userId;
-  const endpointConfig = req.endpointConfig?.config;
+export const cleanup = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    if (req.endpointConfig?.source !== "leveret") {
+        getLogger().simpleLog("info", "Request processed");
+        next();
+        return;
+    }
 
-  if (!endpointConfig) {
-    res.status(500).json({ error: "Endpoint configuration not found" });
-    return;
-  }
+    const endpointConfig = req.endpointConfig?.config;
 
-  if (req.endpointConfig?.source === "leveret") {
+    if (!endpointConfig) {
+        res.status(500).json({ error: "Endpoint configuration not found" });
+        return;
+    }
+
+    const userId = req.body.userId;
     await getDbHandler().updateGlobalRates(endpointConfig.endpointName, "take");
-    await getDbHandler().updateUserRates(
-      userId,
-      endpointConfig.endpointName,
-      "take"
-    );
-  }
+    await getDbHandler().updateUserRates(userId, endpointConfig.endpointName, "take");
 
-  getLogger().simpleLog("info", "Authorized Request");
-  next();
+    getLogger().simpleLog("info", "Authorized Request");
+    next();
 };
